823820770b481f0c6b9561185a62448eaf023862
wpeng20125 <1510350422@qq.com>
2020-10-12 16:18:04 +0800

    初始化项目

---
+++ Musicer/Musicer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Musicer
+//
+//  Created by 王朋 on 2020/10/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    
+}
+
---
+++ Musicer/Musicer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Musicer
+//
+//  Created by 王朋 on 2020/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray
+        
+    }
+}
+
